b478e939d851b5e157046cbb609e76ce9cad667a
SethMcn <107069935+SethMcN@users.noreply.github.com>
2024-11-27 18:42:37 +0000

    Enhance fan project functionality: update button actions for start/stop, add turbo speed option, and improve button styles

--- Pages/Languages/Css/FanProject/rotate.js
+++ Pages/Languages/Css/FanProject/rotate.js
@@ -1,14 +1,51 @@
+let active = false;
+
 function setRotationSpeed(speed) {
-    const img = document.getElementById('FanPic');
-    if (img) {
-        console.log(speed);
+
+    if (active) {
+        const img = document.getElementById('FanPic');
+
         if (img) {
-            img.style.animation = `spin ${speed} linear infinite`;
+            console.log(speed);
+
+            if (img) {
+                img.style.animation = `spin ${speed} linear infinite`;
+            }
+        } else {
+            console.error('Element with id "FanPic" not found.');
         }
-    } else {
-        console.error('Element with id "FanPic" not found.');
+    }
+
+}
+
+function Rotation(action) {
+    const img = document.getElementById('FanPic');
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+
+    active = action;
+
+    if (active) {
+        start.disabled = true;
+        stop.disabled = false;
+        img.style.animation = `spin 10s linear infinite`
+    }
+
+    else {
+        start.disabled = false;
+        stop.disabled = true;
+    }
+
+
+
+
+    if (!active) {
+        img.style.animation = `none`;
     }
 }
+
+
+
 
 // Initial speed
 setRotationSpeed('5s');
